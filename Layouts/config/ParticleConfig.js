ad438864ba45cce1b5918e871b0274cb3f0ef20b
= <=>
2023-12-06 00:26:19 +0530

    Adding Particle Background

---
+++ Layouts/config/ParticleConfig.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ParticleConfig = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default ParticleConfig
--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
-
 const page = () => {
   return (
     <>
       <nav>
-       
+        <button className='bg-yellow-200 p-4' onClick={() => {
+          console.log("click");
+        }
+       }>Click</button>
       </nav>
     </>
   )
