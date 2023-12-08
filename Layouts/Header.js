225aeeaddcfafcc2d08211824a56fbe366cf2a08
= <=>
2023-12-08 21:18:13 +0530

    Modified

--- Layouts/Header.js
+++ Layouts/Header.js
@@ -4,13 +4,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-      <>
-      <div className='top-bar bg-indigo-500 bg-opacity-30 flex mt-2 mx-3  p-3 text-white justify-between rounded-md '>
-        <Link  href={"/"}>Logo</Link>
-        <div>
-          <Link className='p-2' href={"/"}>Projects</Link>
-          <Link className='p-2' href={"/"}>Skills</Link>
-          <Link className='p-2' href={"/"}>Contact</Link>
+    <>
+      <div className='absolute bottom-0 w-full'>
+
+        <div className='bottom-bar bg-indigo-500 bg-opacity-30 flex mb-2 mx-3  p-3 text-white justify-around rounded-md'>
+          
+            <Link className='' href={"/"}>Projects</Link>
+            <Link className='' href={"/"}>Skills</Link>
+            <Link className='' href={"/"}>About</Link>
+            <Link className='' href={"/"}>Contact</Link>
+         
         </div>
       </div>
       </>
